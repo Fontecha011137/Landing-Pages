4aacbb38005edf88e96de009c2e3b8fdfc7c4e94
Fontecha011137 <oh526122@gmail.com>
2026-04-07 06:55:02 +0000

    Agrega validación honeypot y mejora formulario

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-document.getElementById("formLead").addEventListener("submit", function(e){
+document.getElementById("formLead").addEventListener("submit", function(e) {
   const hp = document.getElementById("telefono2").value;
-  if(hp !== ""){
-    // Es un bot, no enviar
+  if (hp !== "") {
+    // Es un bot, prevenir envío
     e.preventDefault();
     console.log("Bot detectado 🚫");
     return;
   }
-  // Si está vacío, el formulario se envía a HoneyPot
+  // Si está vacío, el formulario se envía normalmente
 });
